9066697751e5c707762a09a7cb802bad3979b8e2
Tyler <tfritzy@gmail.com>
2024-03-19 07:18:28 -0700

    WIP implementing inspector

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Bufo } from "./Bufo";
 import { BufoData } from "./BufoData";
 import { BufoInspector } from "./BufoInspector";
-import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   const [inspectedIndex, setInspectedIndex] = React.useState<number | null>(
@@ -10,25 +9,23 @@ function App() {
   );
 
   return (
-    <NextUIProvider>
-      <div className="flex justify-center items-center h-screen bg-gray-100">
-        <div className="w-full max-w-2xl p-4 bg-white shadow-md flex flex-row flex-wrap max-h-screen overflow-y-scroll">
-          {BufoData.map((bufo, index) => (
-            <Bufo
-              key={bufo.name}
-              name={bufo.name}
-              fileExtension={bufo.type}
-              onClick={() => setInspectedIndex(index)}
-            />
-          ))}
-        </div>
-        <BufoInspector
-          bufo={BufoData[inspectedIndex || 0]}
-          onClose={() => setInspectedIndex(null)}
-          isOpen={inspectedIndex !== null}
-        />
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="w-full max-w-2xl p-4 bg-white shadow-md flex flex-row flex-wrap max-h-screen overflow-y-scroll">
+        {BufoData.map((bufo, index) => (
+          <Bufo
+            key={bufo.name}
+            name={bufo.name}
+            fileExtension={bufo.type}
+            onClick={() => setInspectedIndex(index)}
+          />
+        ))}
       </div>
-    </NextUIProvider>
+      <BufoInspector
+        bufo={BufoData[inspectedIndex || 0]}
+        onClose={() => setInspectedIndex(null)}
+        isOpen={inspectedIndex !== null}
+      />
+    </div>
   );
 }
 
--- src/BufoInspector.tsx
+++ src/BufoInspector.tsx
@@ -8,26 +8,54 @@ type BufoInspectorProps = {
   isOpen: boolean;
 };
 
+const Updoot = (props: { bufo: BufoDetails }) => {
+  const updootCount = Math.floor(Math.random() * 10) + 1;
+  return (
+    <div className="rounded-md bg-gray-100 flex flex-row items-center space-x-1 w-min px-[6px] py-[2px]">
+      <img
+        src={`/all-the-bufo/${props.bufo.name}.${props.bufo.type}`}
+        alt={props.bufo.name}
+        className="min-w-[18px] min-h-[18px]"
+      />
+      <span className="text-sm">{updootCount}</span>
+    </div>
+  );
+};
+
 export const BufoInspector = (props: BufoInspectorProps) => {
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
-      <div className="p-3">
-        <div className="flex flex-row space-x-4">
+      <div className="p-3 w-max">
+        <div className="flex flex-row space-x-2">
           <img
             src={`/all-the-bufo/${props.bufo.name}.${props.bufo.type}`}
             alt={props.bufo.name}
-            className="rounded-lg w-[64px] h-[64px] border border-lime-400 shadow-md shadow-lime-50 bg-lime-50"
+            className="rounded-lg w-[48px] h-[48px] shadow-md shadow-lime-50 bg-gray-200"
           />
 
-          <div>
-            <span className="font-bold">{props.bufo.name}</span>{" "}
-            <span className="text-gray-600 text-sm">
-              {new Date().toLocaleTimeString("en-US", {
-                hour: "numeric",
-                minute: "numeric",
-              })}
-            </span>
-            <div>In a complex world, be simply amphibious.</div>
+          <div className="flex flex-col space-y-1">
+            <div className="flex flex-row items-center space-x-1 items-center">
+              <div className="font-semibold">{props.bufo.name}</div>
+              <div className="text-gray-600 text-xs">
+                {new Date().toLocaleTimeString("en-US", {
+                  hour: "numeric",
+                  minute: "numeric",
+                })}
+              </div>
+            </div>
+
+            <div className="flex flex-row space-x-1 items-center">
+              <span>In a complex world, be simply amphibious </span>
+              <span>
+                <img
+                  src={`/all-the-bufo/${props.bufo.name}.${props.bufo.type}`}
+                  alt={props.bufo.name}
+                  className="w-[16px] h-[16px]"
+                />
+              </span>
+            </div>
+
+            <Updoot bufo={props.bufo} />
           </div>
         </div>
 
