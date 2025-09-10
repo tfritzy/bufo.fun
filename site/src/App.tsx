817938105caecfb6777b4c643272bb4995fa4c1a
Tyler Fritz <tfritzy@gmail.com>
2025-09-10 08:39:14 -0500

    bit wider

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -11,24 +11,10 @@ function App() {
   Set<string>
  >(new Set());
 
- if (BufoData.length === 0) {
-  return (
-   <div className="w-screen h-screen flex flex-col justify-center items-center">
-    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-bufo-400">
-     <img
-      src="/bufos/bufo-late-to-the-convo.png"
-      width="32px"
-      height="32px"
-      aria-label="loading"
-     />
-    </div>
-   </div>
-  );
- }
 
  return (
   <div className="overflow-y-scroll min-h-screen max-h-screen bg-gray-100">
-   <div className="flex flex-col items-start max-w-3xl m-auto">
+   <div className="flex flex-col items-start max-w-4xl m-auto">
     <Header />
 
     <div className="mx-2 mb-2">
