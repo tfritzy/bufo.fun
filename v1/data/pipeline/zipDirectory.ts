0ad66df0daadaf422a4d7d3aeca92c267b517b32
Tyler <tfritzy@gmail.com>
2026-01-25 14:28:39 -0600

    refactor site

--- v1/data/pipeline/zipDirectory.ts
+++
@@ -1,6 +0,0 @@
-export function zipDirectory(sourceDir: string, outPath: string) {
-  const child_process = require("child_process");
-  child_process.execSync(`zip -r ${outPath} *`, {
-    cwd: sourceDir,
-  });
-}
